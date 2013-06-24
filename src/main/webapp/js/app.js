e3dd68080649021524a087f0da8c28b4983d1587
Jim Klucar <klucar@gmail.com>
2013-06-23 21:38:57 -0400

    added various js/css libraries, index.html is a working test of libs

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,7 +2,12 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+angular.module('accumuloApp', ['accumuloApp.filters', 
+                               'accumuloApp.services', 
+                               'accumuloApp.directives', 
+                               'accumuloApp.controllers', 
+                               'placeholders', 
+                               'ui.bootstrap']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
